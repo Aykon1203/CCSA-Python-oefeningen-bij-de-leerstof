a39d4fe62d9d8934368eddffb644b26e762fe3e6
Aykon Kirhan <aykon.kirhan@student.hogent.be>
2025-09-30 12:13:39 +0200

    Implement binary search function and example usage

--- h1_zoeken_en_sorteren/iteratieve_versie_binair_zoeken.py
+++ h1_zoeken_en_sorteren/iteratieve_versie_binair_zoeken.py
@@ -1,3 +1,16 @@
+def zoek_binair(rij,getal):
+  links=0
+  rechts=len(rij)-1
+  while links!=rechts:
+    midden = (links+rechts)//2 #doet zelfde als floor(), maar zonder import
+    if rij[midden]<getal: #getal = te zoeken item in de array
+      links = midden+1 #zoeken in rechterhelft
+    else: 
+      rechts=midden #zoeken linkerhelft inclusief midden
+  if rij[links]==getal: #links gelijk aan rechts -> getal is gevonden want 1 element over
+    return links
+  else:
+    return -1;
 
-def zoekBinair():
-  pass
+mijn_rij = [0,10,20,30,40,50,60,70,80,90]
+print(zoek_binair(mijn_rij,70))
