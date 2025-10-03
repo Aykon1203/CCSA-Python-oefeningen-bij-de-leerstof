cf68ea9a626f1dbbd0d655f7ab1843333b1ff162
Aykon Kirhan <aykon.kirhan@student.hogent.be>
2025-10-03 14:18:00 +0200

    selection sort

--- h1_zoeken_en_sorteren/sorteren_door_selectie.py
+++ h1_zoeken_en_sorteren/sorteren_door_selectie.py
@@ -1,7 +1,22 @@
-def selection_sort_vooraan(a):
+def selection_sort_vooraan(rij):
     pass
+
+def selection_sort(rij):
+    n=len(rij)
+    for i in range(n-1,0,-1):
+        max=rij[i]
+        max_pos=i
+        for j in range(i-1,-1,-1):
+            if rij[j]>max:
+                #nieuw max gevonden
+                max=rij[j]
+                max_pos=j
+            #max achteraan plaatsen
+        rij[i],rij[max_pos]=max,rij[i]
+        print(rij)
 
 
 a = [int(_) for _ in input().split()]
-selection_sort_vooraan(a)
+selection_sort(a)
+# selection_sort_vooraan(a)
 
